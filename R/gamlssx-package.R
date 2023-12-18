b7c4ea6aa82e7ede7f4ca62893a43c76ad4cccb7
Paul Northrop <p.northrop@ucl.ac.uk>
2023-12-18 16:46:25 +0000

    Initial commit

---
+++ R/gamlssx-package.R
@@ -0,0 +1,11 @@
+#' gamlssx: Generalised Additive Extreme Value Models for Location, Scale and
+#' Shape
+#'
+#' Describe
+#'
+#' @details More details.
+#' @references Rigby R.A. and Stasinopoulos D.M. (2005). Generalized additive
+#'   models for location, scale and shape (with discussion), *Appl. Statist.*,
+#'   **54**, part 3, pages 507-554. \doi{10.1111/j.1467-9876.2005.00510.x}
+#' @docType package
+"_PACKAGE"
