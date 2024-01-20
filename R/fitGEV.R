8b620c8c07bf5ddabe590e46633516c037b03550
Paul Northrop <p.northrop@ucl.ac.uk>
2024-01-20 18:48:12 +0000

    Wrap all calls to gamlss() with try(). Add flexibility.

--- R/fitGEV.R
+++ R/fitGEV.R
@@ -3,19 +3,24 @@
 #' Describe  [`GEV`]
 #'
 #' @param formula Argument `formula` passed to [`gamlss`][`gamlss::gamlss`].
-#' @param stepLength A positive numeric scalar. The initial value of the common
-#'  step lengths `mu.step`, `sigma.step` and `nu.step` passed to
+#' @param stepLength A numeric vector containing positive values. The initial
+#'   values of the step lengths `mu.step`, `sigma.step` and `nu.step` passed to
 #'  [`gamlss.control`][`gamlss::gamlss.control`] in the first attempt to fit
-#'  the model by calling [`gamlss`][`gamlss::gamlss`].
-#' @param stepReduce A non-negative integer. If the first call to
-#'  [`gamlss`][`gamlss::gamlss`] throws an error then we make `stepReduce`
-#'  further attempts to fit the model, each time dividing by \eqn{2} the values
+#'  the model by calling [`gamlss`][`gamlss::gamlss`]. If `stepLength` has a
+#'  length that is less than 3 then `stepLength` is recycled to have length 3.
+#' @param stepAttempts A non-negative integer. If the first call to
+#'  [`gamlss`][`gamlss::gamlss`] throws an error then we make `stepAttempts`
+#'  further attempts to fit the model, each time dividing by 2 the values
 #'  of `mu.step`, `sigma.step` and `nu.step` supplied to
-#'  [`gamlss.control`][`gamlss::gamlss.control`]. If `stepReduce < 1` then
+#'  [`gamlss.control`][`gamlss::gamlss.control`]. If `stepAttempts < 1` then
 #'  no further attempts are made.
+#' @param stepReduce A number greater than 1. The factor by which the step
+#'   lengths in `stepLength` are reduced for each extra attempt to fit the
+#'   model. The default, `stepReduce = 2` means that the step lengths are
+#'   halved for each extra attempt.
 #' @param ... Further arguments passed to [`gamlss`][`gamlss::gamlss`].
 #'
-#' @details Add details. Explain `stepReduce` in more detail.
+#' @details Add details. Explain `stepAttempts` in more detail.
 #'
 #' @return Returns a gamlss object. See the **Value** section of
 #'   [`gamlss`][`gamlss::gamlss`]. The class of the returned object is
@@ -49,20 +54,21 @@
 #' # Fit model using the CG method
 #' mod <- fitGEV(y ~ pb(x), data = data, method = CG())
 #' @export
-fitGEV <- function(formula, stepLength = 1, stepReduce = 2, ...)  {
+fitGEV <- function(formula, stepLength = 1, stepAttempts = 2, stepReduce = 2,
+                   ...) {
   mod <- try(gamlss(y ~ pb(x), family = GEV, mu.step = stepLength,
                     sigma.step = stepLength, nu.step = stepLength, ...),
              silent = TRUE)
-  # If an error is thrown then try again stepReduce times, each  time reducing
+  # If an error is thrown then try again stepAttempts times, each  time reducing
   # the step length by a factor of a half
   isError <- inherits(mod, "try-error")
-  while(isError & stepReduce >= 1) {
-    stepLength <- stepLength / 2L
-    mod <- gamlss(y ~ pb(x), family = GEV, mu.step = stepLength,
-                  sigma.step = stepLength, nu.step = stepLength, ...)
+  while(isError & stepAttempts >= 1) {
+    stepLength <- stepLength / stepReduce
+    mod <- try(gamlss(y ~ pb(x), family = GEV, mu.step = stepLength,
+                      sigma.step = stepLength, nu.step = stepLength, ...),
+               silent = TRUE)
     isError <- inherits(mod, "try-error")
-    stepReduce <- stepReduce - 1L
-    stepLength <- stepLength / 2
+    stepAttempts <- stepAttempts - 1L
   }
   class(mod) <- c("gamlssx", class(mod))
   return(mod)
