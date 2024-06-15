c28beda9d255181b0fcf4728726e74d38ee7e018
Paul Northrop <p.northrop@ucl.ac.uk>
2024-06-15 19:59:58 +0100

    Rename

--- R/GEVfamilyQuasiNewtonScoring.R
+++ R/GEVfamilyQuasiNewtonScoring.R
@@ -74,8 +74,8 @@ NULL
 
 #' @rdname GEV
 #' @export
-GEVquasi <- function(mu.link = "identity", sigma.link = "log",
-                     nu.link = "identity") {
+GEVquasiNewton <- function(mu.link = "identity", sigma.link = "log",
+                           nu.link = "identity") {
 
   mstats <- gamlss.dist::checklink("mu.link", "GEV", substitute(mu.link),
                                    c("1/mu^2", "log", "identity"))
