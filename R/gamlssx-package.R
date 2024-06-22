ca23a197a4ce9649e2b8a84af667e5a34aa68989
Paul Northrop <p.northrop@ucl.ac.uk>
2024-06-22 14:41:53 +0100

    Import sd() from stats

--- R/gamlssx-package.R
+++ R/gamlssx-package.R
@@ -7,5 +7,6 @@
 #' @references Rigby R.A. and Stasinopoulos D.M. (2005). Generalized additive
 #'   models for location, scale and shape (with discussion), *Appl. Statist.*,
 #'   **54**, part 3, pages 507-554. \doi{10.1111/j.1467-9876.2005.00510.x}
+#' @importFrom stats sd
 #' @docType package
 "_PACKAGE"
