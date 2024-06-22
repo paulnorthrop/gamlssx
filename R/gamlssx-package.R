24966c922cad3e169dce508712bd62dedf39a196
Paul Northrop <p.northrop@ucl.ac.uk>
2024-06-22 23:45:01 +0100

    Package help file

--- R/gamlssx-package.R
+++ R/gamlssx-package.R
@@ -1,12 +1,20 @@
 #' gamlssx: Generalised Additive Extreme Value Models for Location, Scale and
 #' Shape
 #'
-#' Describe
+#' Functions for fitting generalized additive models for the location, scale
+#' and shape parameters of a generalized extreme value distribution. The
+#' methodology is based on Rigby and Stasinopoulos (2005) and implemented using
+#' functions from the `gamlss` package \doi{10.32614/CRAN.package.gamlss}.
 #'
-#' @details More details.
+#' @details The main function in `gamlssx` is [`fitGEV`], which calls the
+#'   function [`gamlss`][`gamlss::gamlss`].
+#'   See the \href{https://paulnorthrop.github.io/gamlssx/}{gamlssx package
+#'   page on Github} for more information.
+#'
 #' @references Rigby R.A. and Stasinopoulos D.M. (2005). Generalized additive
 #'   models for location, scale and shape (with discussion), *Appl. Statist.*,
 #'   **54**, part 3, pages 507-554. \doi{10.1111/j.1467-9876.2005.00510.x}
+#' @seealso [`fitGEV`], [`gamlss`][`gamlss::gamlss`]
 #' @importFrom stats sd
 #' @docType package
 "_PACKAGE"
