3dcf31f9fc4126e24be94dae448e46be3a74e58b
Paul Northrop <p.northrop@ucl.ac.uk>
2024-06-22 11:26:59 +0100

    Deal with weird test error

--- tests/testthat/test-basicGEVfit.R
+++ tests/testthat/test-basicGEVfit.R
@@ -4,31 +4,39 @@ tol <- 1e-4
 
 # gamlss
 
-mod <- fitGEV(SeaLevel ~ 1, data = fremantle)
+mod <- gamlssx::fitGEV(SeaLevel ~ 1, data = fremantle)
 # Estimated coefficients
-muhat <- coef(mod, what = "mu")
-sigmahat <- exp(coef(mod, what = "sigma"))
-xihat <- coef(mod, what = "nu")
+muhat <- as.numeric(mod$mu.coefficients) # 1.482304
+sigmahat <- as.numeric(exp(mod$sigma.coefficients)) # 0.1412218
+xihat <- as.numeric(mod$nu.coefficients) # -0.2172112
 # Fit again using these as starting estimates
-mod <- fitGEV(SeaLevel ~ 1, data = fremantle, mu.start = muhat,
-              sigma.start = sigmahat, nu.start = xihat)
+# I pass the numeric values for mu.start etc because when the next line of
+# code is run outside of test_that(), using devtools::check(), if I pass
+# mu.start = muhat then the value of muhat is not passed correctly to
+# templatefit(...) in fitGEV() and an error that says that muhat is not found
+# is thrown. It works fine using devtools::test() and when executing code via
+# the command line. I'm not entirely sure what the problem is - environments,
+# presumbly. This is a quick fix.
+mod <- gamlssx::fitGEV(SeaLevel ~ 1, data = fremantle, mu.start = 1.482304,
+                       sigma.start = 0.1412218, nu.start = -0.2172112)
 # Estimated coefficients
-muhat <- coef(mod, what = "mu")
-sigmahat <- exp(coef(mod, what = "sigma"))
-xihat <- coef(mod, what = "nu")
+muhat <- mod$mu.coefficients
+sigmahat <- exp(mod$sigma.coefficients)
+xihat <- mod$nu.coefficients
 # Estimates, SEs, maximised log-likelihood
 gamlssEstimates <- as.numeric(c(muhat, sigmahat, xihat))
 ses <- sqrt(diag(vcov(mod)))
-ses[2] <- ses[2] * estimates[2]
+ses[2] <- ses[2] * gamlssEstimates[2]
 gamlssSEs <- as.numeric(ses)
 gamlssLoglik <- as.numeric(logLik(mod))
 
 # ismev
 
-x <- ismev::gev.fit(xdat = fremantle$SeaLevel)
-x$mle
-x$se
--x$nllh
+#x <- ismev::gev.fit(xdat = fremantle$SeaLevel)
+#x$mle
+#x$se
+#-x$nllh
+
 ismevEstimates <- c(1.4823409,  0.1412671, -0.2174320)
 ismevSEs <- c(0.01672502, 0.01149461, 0.06377394)
 ismevLoglik <- 43.56663
