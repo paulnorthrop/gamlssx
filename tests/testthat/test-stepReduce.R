6847c34383c781b72dce4db103499315e4e096ff
Paul Northrop <p.northrop@ucl.ac.uk>
2024-06-22 15:07:34 +0100

    Need gamlss::pb() not just pb()

--- tests/testthat/test-stepReduce.R
+++ tests/testthat/test-stepReduce.R
@@ -11,27 +11,28 @@ mu <- 1 + 2 * x
 sigma <- 1
 xi <- 0.25
 y <- nieve::rGEV(n = 1, loc = mu, scale = sigma, shape = xi)
-plot(x, y)
 data <- data.frame(y = as.numeric(y), x = x)
 
 # Fit model using the default RS method
-modRS <- fitGEV(y ~ pb(x), data = data)
+modRS <- fitGEV(y ~ gamlss::pb(x), data = data)
+print(modRS)
 muhat <- modRS$mu.coefficients
 sigmahat <- exp(modRS$sigma.coefficients)
 xihat <- modRS$nu.coefficients
 RSestimates <- as.numeric(c(muhat, sigmahat, xihat))
-# 1.2726165, 1.3830108 0.9161198 0.1926386
+loglikRS <- logLik(modRS)
 
 # Fit model using the CG method
-modCG <- fitGEV(y ~ pb(x), data = data, method = CG())
+modCG <- fitGEV(y ~ gamlss::pb(x), data = data, method = CG())
 muhat <- modCG$mu.coefficients
 sigmahat <- exp(modCG$sigma.coefficients)
 xihat <- modCG$nu.coefficients
 CGestimates <- as.numeric(c(muhat, sigmahat, xihat))
+loglikCG <- logLik(modCG)
 
 
 test_that("RS logLik equals CG logLik", {
-  testthat::expect_equal(logLik(modRS), logLik(modCG), tolerance = tol)
+  testthat::expect_equal(loglikRS, loglikCG, tolerance = tol)
 })
 
 test_that("RS estimates equal CG estimates", {
